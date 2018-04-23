768be4ac884c22d4c558478f698ce2062e64f5f2
mpsingh17 <mpsingh17@outlook.co.nz>
2018-04-23 20:14:28 +1200

    Managing evs variables with .env package

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express  = require('express'),
-      Joi      = require('joi'),
-      mongoose = require('mongoose'),
-      config   = require('config'),
-      genres   = require('./routes/genres') ;
+require('dotenv').config() ;
+
+const express     = require('express'),
+      Joi         = require('joi'),
+      mongoose    = require('mongoose'),
+      config      = require('config'),
+      appDebugger = require('debug')('app:startup'),
+      dbDebugger  = require('debug')('app:db'),
+      genres      = require('./routes/genres') ;
 
 const app = express() ;
 
+// process.env.NODE_ENV = 'development' ;
 //---------- Connect to DB -----------------------//
 if ( process.env.NODE_ENV === 'development' ) {
   mongoose
     .connect( 'mongodb://' + config.get('dbConfig.hostName') + '/' + config.get('dbConfig.dbName') )
-    .then( () => { console.log(`Connected to ${config.get('dbConfig.dbName')}...`) } )
-    .catch( (err) => { console.error('Could not connect...', err) } ) ;
+    .then( () => { dbDebugger(`Connected to ${config.get('dbConfig.dbName')}...`) } )
+    .catch( (err) => { dbDebugger('Could not connect...', err) } ) ;
 }
 
 //------------------------------------- Middlewares ------------------------------//
@@ -22,5 +27,5 @@ app.use('/api/genres', genres) ;
 
 // Ruuning app at 3000 port.
 app.listen( config.get('port'), () => {
-  console.log(`Server listening at ${config.get('port')}...`) ;
+  appDebugger(`Server listening at localhost:${config.get('port')}...`) ;
 } ) ;
