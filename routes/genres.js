1b47c41b0f7a78550bcba4eda3aa6a5ad96d2c43
mpsingh17 <mpsingh17@outlook.co.nz>
2018-04-25 11:02:24 +1200

    Simplify async route handlers.

--- routes/genres.js
+++ routes/genres.js
@@ -17,98 +17,84 @@ const genres = [
  * GET: /api/genres
  * It will send array of objects of genres
  */
-router.get('/', (req, res) => {
-  async function getAllGenres() {
-    try {
-      const genres = await Genre.find() ;
-      return res.send(genres) ;
-    }
-    catch(err) {
-      return res.status(404).send(err) ;
-    }
+router.get('/', async (req, res) => {
+  try {
+    const genres = await Genre.find() ;
+    return res.send(genres) ;
   }
-  getAllGenres() ;
+  catch(err) {
+    return res.status(404).send(err) ;
+  }
 }) ;
 
 /**
  * GET: /api/genres/{id}
  * It will send object of single genre
  */
-router.get('/:id', (req, res) => {
-  async function getGenreByID() {
-    try {
-      const genre = await Genre.findById(req.params.id) ;
-      return res.send(genre) ;
-    } 
-    catch(err) {
-      return res.status(404).send(err.message) ;
-    }
+router.get('/:id', async (req, res) => {
+  try {
+    const genre = await Genre.findById(req.params.id) ;
+    return res.send(genre) ;
+  } 
+  catch(err) {
+    return res.status(404).send(err.message) ;
   }
-  getGenreByID() ;
 }) ;
 
 /**
  * POST: /api/genres
  * It will create a genre and push it to genres array.
  */
-router.post('/', (req, res) => {
-  async function processPostReq() {
-    try {
-      const validReqData = await validateReqBody(req.body) ;
-      genreDebugger('valid req data is ' , validReqData) ;
+router.post('/', async (req, res) => {
+  try {
+    const validReqData = await validateReqBody(req.body) ;
+    genreDebugger('valid req data is ' , validReqData) ;
 
-      const genre = new Genre( {
-        name: validReqData.name
-      } ) ;
-      const result = await genre.save() ;
-      return res.send(result) ;
-    }
-    catch(error) {
-      genreDebugger('error is ' , error.message) ;
-      return res.send(error.message) ;
-    }
+    const genre = new Genre( {
+      name: validReqData.name
+    } ) ;
+    const result = await genre.save() ;
+    return res.send(result) ;
   }
-  processPostReq() ;
+  catch(error) {
+    genreDebugger('error is ' , error.message) ;
+    return res.send(error.message) ;
+  }
 }) ;
 
 /**
  * PUT: /api/genres/{id}
  * It will update a genre object if valid.
  */
-router.put('/:id', (req, res) => {
-  async function processPutReq() {
-    try {
-      const validReqData = await validateReqBody(req.body) ;
-      const genre        = await Genre.findById(req.params.id) ;
-      genreDebugger(genre, ' --> ' , validReqData) ;
-      genre.set( {
-        name: validReqData.name
-      } ) ;
-      genre.save() ;
-      genreDebugger(genre, ' --> ' , validReqData) ;
-      return res.send(genre) ;
-    } 
-    catch(error) {
-      return res.send(error.message) ;
-    }
+router.put('/:id', async (req, res) => {
+  try {
+    const validReqData = await validateReqBody(req.body) ;
+    const genre        = await Genre.findById(req.params.id) ;
+    genreDebugger(genre, ' --> ' , validReqData) ;
+    genre.set( {
+      name: validReqData.name
+    } ) ;
+    genre.save() ;
+    genreDebugger(genre, ' --> ' , validReqData) ;
+    return res.send(genre) ;
+  } 
+  catch(error) {
+    return res.send(error.message) ;
   }
-  processPutReq() ;
 } ) ;
 
 /**
  * DELETE: /api/genres/{id}
  */
-router.delete('/:id', (req, res) => {
-  async function deleteGenre() {
-    try {
-      const genre = await Genre.findByIdAndRemove( req.params.id ) ;
-      genreDebugger('genre is -->', genre) ;
-      return (genre === null) ? res.status(404).send('Genre not found.') : res.send(`${genre.name} genre has been deleted.`);
-    } catch (error) {
-      return res.status(404).send(error.message) ;
-    }
+router.delete('/:id', async (req, res) => {
+  try {
+    const genre = await Genre.findByIdAndRemove( req.params.id ) ;
+    genreDebugger('genre is -->', genre) ;
+    return (genre === null) ? res.status(404).send('Genre not found.') : res.send(`${genre.name} genre has been deleted.`);
   }
-  deleteGenre() ;
+  catch (error) {
+    return res.status(404).send(error.message) ;
+  }
 } ) ;
 
 //------------------ Helper function ------------------------//
