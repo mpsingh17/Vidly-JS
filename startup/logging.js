163164efd3a00db093403bfa5b2b9fd738b95fd0
mpsingh17 <mpsingh17@outlook.co.nz>
2018-04-30 18:51:13 +1200

    Enable exception handling method to print on console.

--- startup/logging.js
+++ startup/logging.js
@@ -4,7 +4,10 @@ const winston = require('winston') ;
 module.exports = function() {
   winston.add(winston.transports.File, {filename: '../logs/winston.log'}) ;
   
-  winston.handleExceptions(new winston.transports.File({filename: './logs/winston.log'})) ;
+  winston.handleExceptions(
+    new winston.transports.Console({colorize: true, prettyPrint: true}),
+    new winston.transports.File({filename: './logs/winston.log'})
+  ) ;
   
   process.on('unhandledRejection', (ex) => {
     throw ex ;
