5cbab2b3ef432c5c452730b4dcf009d88035eeae
mpsingh17 <mpsingh17@outlook.co.nz>
2018-04-30 18:47:16 +1200

    Validation logic moved to seprate module.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config() ;
 
-const express     = require('express'),
-      mongoose    = require('mongoose'),
-      config      = require('config'),
-      winston     = require('winston') ;
+const express = require('express'),
+      config  = require('config'),
+      winston = require('winston') ;
 
 const app = express() ;
 
@@ -11,8 +10,7 @@ require('./startup/logging')() ;
 
 require('./startup/db')() ;
 require('./startup/routes')(app) ;
+require('./startup/validation')() ;
 
 // Running app at given port.
-app.listen( config.get('port'), () => {
-  winston.info(`Server listening at localhost:${config.get('port')}...`) ;
-} ) ;
+app.listen( config.get('port'), () => winston.info(`Server listening at localhost:${config.get('port')}...`) ) ;
