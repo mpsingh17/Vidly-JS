57ca02040064fd255caaa9bf908b1f3eb196fdd7
mpsingh17 <mpsingh17@outlook.co.nz>
2018-04-30 18:35:04 +1200

    Error handling and logging logic moved to seprate module.

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 require('dotenv').config() ;
-require('express-async-errors') ;
 
 const express     = require('express'),
       mongoose    = require('mongoose'),
       config      = require('config'),
-      appDebugger = require('debug')('app:startup'),
       winston     = require('winston') ;
 
 const app = express() ;
 
-winston.add(winston.transports.File, {filename: './logs/winston.log'}) ;
-
-winston.handleExceptions(new winston.transports.File({filename: './logs/winston.log'})) ;
-
-process.on('unhandledRejection', (ex) => {
-  throw ex ;
-}) ;
+require('./startup/logging')() ;
 
 require('./startup/db')() ;
 require('./startup/routes')(app) ;
 
 // Running app at given port.
 app.listen( config.get('port'), () => {
-  appDebugger(`Server listening at localhost:${config.get('port')}...`) ;
+  winston.info(`Server listening at localhost:${config.get('port')}...`) ;
 } ) ;
---
+++ startup/logging.js
@@ -0,0 +1,12 @@
+require('express-async-errors') ;
+const winston = require('winston') ;
+
+module.exports = function() {
+  winston.add(winston.transports.File, {filename: '../logs/winston.log'}) ;
+  
+  winston.handleExceptions(new winston.transports.File({filename: './logs/winston.log'})) ;
+  
+  process.on('unhandledRejection', (ex) => {
+    throw ex ;
+  }) ;
+}
